227171bd01d64a79461467f94012aab8d8c4eba8
amarchenko <amarchenko@playtika.com>
2019-08-13 17:20:00 +0300

    first commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+//        System.out.println("Дратути");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter x:");
+        int x = scanner.nextInt();
+
+        System.out.println("Enter y:");
+        int y = scanner.nextInt();
+
+        System.out.printf("%d * %d = %d", x, y, x * y);
+    }
+}
